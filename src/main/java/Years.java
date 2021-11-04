868fe38c8e1cc4361f70d965942f9a8be536b184
topetopetope <thomaskvaremp@gmail.com>
2021-11-04 16:36:30 +0100

    First Commit

--- src/main/java/Years.java
+++ src/main/java/Years.java
@@ -1,15 +1,15 @@
 public class Years {
 
     public static boolean isLeapYear(int year) {
-        boolean thisIsARealLeapYear;
+        boolean thisIsAActualLeapYear;
 
         boolean dividableBy4 = year % 4 == 0;
         boolean notDividableBy100 = year % 100 != 0;
         boolean dividableBy400 = year % 400 == 0;
 
 
-        thisIsARealLeapYear = dividableBy4 && notDividableBy100 || dividableBy400;
+        thisIsAActualLeapYear = dividableBy4 && notDividableBy100 || dividableBy400;
 
-        return thisIsARealLeapYear;
+        return thisIsAActualLeapYear;
     }
 }
